15727a677db419fd92ab01dad5dc177e1db9834a
Anura Mohaniya <anura.mohaniya@gmail.com>
2025-10-11 13:23:46 +0530

    review/status/requestId api in connection request

--- src/routes/request.js
+++ src/routes/request.js
@@ -58,4 +58,48 @@ requestRouter.post(
   }
 );
 
+requestRouter.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const { status, requestId } = req.params;
+      const loggedInUser = req.user;
+
+      //Suppose Anura has sent "interested" request to Elon
+      //requestId:: id of the user who has sent the request (Anura) [ObjectId of the connection request] (68e9fad0505f944ba7556abd)
+      //loggedInUser id:: id of the user who has received the request (Elon)
+
+      const allowedStatus = ["accepted", "rejected"];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({ message: "Status invalid" });
+      }
+
+      const connectionRequest = await ConnectionRequest.findOne({
+        _id: requestId,
+        toUserId: loggedInUser._id,
+        status: "interested",
+      });
+
+      if (!connectionRequest) {
+        return res
+          .status(404)
+          .json({ message: "Connection request not found" });
+      }
+
+      connectionRequest.status = status;
+
+      const data = await connectionRequest.save();
+      res.json({
+        message: "Connection request " + status,
+        //toUserId.firstName + " is " + status + " by " + req.user.firstName,
+        data,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(400).send("ERR: " + err.message);
+    }
+  }
+);
+
 module.exports = requestRouter;
